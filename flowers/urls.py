5dd940c8dc09b19ff32d00da08eadd2bd0b5b580
katwu90 <katwu90@gmail.com>
2017-12-23 18:46:51 -0500

    Adds flowers/:id urls
    Adds a view for flowers/:id url end point.

--- flowers/urls.py
+++ flowers/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	# /flowers/
     url(r'^$', views.index, name='index'),
+
+    # /flowers/50/
+    url(r'^(?P<flower_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- flowers/views.py
+++ flowers/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>This is the flowers home page</h1>")
+
+def detail(request, flower_id):
+	return HttpResponse("<h2>Details for flower:" + str(flower_id) + "</h2>")
+
