fca31d627de8249d81d2a42133b32cf3a12a14e3
katwu90 <katwu90@gmail.com>
2017-12-23 18:41:18 -0500

    Debugs price to Flower model
    The price column in Flower model was missing parenthesis and it was
    fixed by adding it in.

--- flowers/models.py
+++ flowers/models.py
@@ -6,5 +6,9 @@ from django.db import models
 class Flower(models.Model):
     name = models.CharField(max_length=250)
     description = models.CharField(max_length=1000)
-    price = models.IntegerField
+    price = models.IntegerField()
     picture = models.CharField(max_length=1000)
+
+    def __str__(self):
+    	return self.name + ' - ' + self.description
+ 
